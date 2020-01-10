88d1abf034af41d21feea41bdc23e83f1060cb10
Tommy Jones <jones.thos.w@gmail.com>
2020-01-09 22:30:43 -0500

    clean up documentation

--- R/tidylda.R
+++ R/tidylda.R
@@ -6,7 +6,7 @@
 #' using style conventions from the 'tidyverse' and specifically 'tidymodels'. 
 #' Designed to work seamlessly with 'textmineR'.
 #'
-#' @name tidylda
+#' @name tidylda-package
 #' @docType package
 NULL
 
@@ -14,10 +14,10 @@ NULL
 #' @import Matrix
 #' @import Rcpp
 #' @import textmineR
-#' @importFrom stats median
 #' @importFrom gtools rdirichlet
-#' @importFrom Rcpp sourceCpp
 #' @importFrom methods as
+#' @importFrom Rcpp sourceCpp
+#' @importFrom stats median
 #' @useDynLib "tidylda", .registration=TRUE
 NULL
 

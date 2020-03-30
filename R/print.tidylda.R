570dd1a91fd875b55b89a38d1cf7072cfb68007a
Tommy Jones <jones.thos.w@gmail.com>
2020-03-29 22:33:47 -0400

    print fewer lines by default

--- R/print.tidylda.R
+++ R/print.tidylda.R
@@ -2,6 +2,7 @@
 #' @description Print a summary for objects of class \code{tidylda}
 #' @param x an object of class \code{tidylda}
 #' @param digits minimal number of significant digits
+#' @param n Number of rows to show in each displayed \code{\link[tibble]{tibble}}.
 #' @param ... further arguments passed to or from other methods
 #' @examples
 #' \donttest{
@@ -16,7 +17,7 @@
 #' print(lda, digits = 2)
 #' }
 #' @export
-print.tidylda <- function(x, digits = max(3L, getOption("digits") - 3L), ...) {
+print.tidylda <- function(x, digits = max(3L, getOption("digits") - 3L), n = 5, ...) {
 
   # make this fail elegantly for legacy topic model objects
 
@@ -40,15 +41,15 @@ print.tidylda <- function(x, digits = max(3L, getOption("digits") - 3L), ...) {
     cat("The model's R-squared is ", round(x$r2, digits = digits), "\n")
   }
 
-  cat("The most prevalent topics are:\n")
+  cat("The ", n, " most prevalent topics are:\n")
 
-  print(s[order(s$prevalence, decreasing = TRUE), ])
+  print(s[order(s$prevalence, decreasing = TRUE), ], n = n)
 
   cat("\n")
 
-  cat("The most coherent topics are:\n")
+  cat("The ", n, " most coherent topics are:\n")
 
-  print(s[order(s$coherence, decreasing = TRUE), ])
+  print(s[order(s$coherence, decreasing = TRUE), ], n = n)
 
 
 

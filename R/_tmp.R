22499821130e583fdb613a3c80db5c92bdfedc82
Tommy <jones.thos.w@gmail.com>
2020-07-01 23:08:28 -0400

    using my sandbox file to write the gibbs sampler in R

--- R/_tmp.R
+++ R/_tmp.R
@@ -1,83 +1,128 @@
 ################################################################################
-# Experimenting with a different (better?) way to get counts for model updates
+# This script is a simple LDA gibbs sampler in R
+# I'm using it to help troubleshoot and optimize my C++ code
 ################################################################################
 
-
-library(tidytext)
-library(tidyverse)
 library(tidylda)
-library(Matrix)
-### Initial set up ---
-# load some documents
-docs <- textmineR::nih_sample
-# tokenize using tidytext's unnest_tokens
-tidy_docs <- docs %>%
-  select(APPLICATION_ID, ABSTRACT_TEXT) %>%
-  unnest_tokens(output = word,
-                input = ABSTRACT_TEXT,
-                stopwords = stop_words$word,
-                token = "ngrams",
-                n_min = 1, n = 2) %>%
-  count(APPLICATION_ID, word) %>%
-  filter(n>1) #Filtering for words/bigrams per document, rather than per corpus
-tidy_docs <- tidy_docs %>% # filter words that are just numbers
-  filter(! stringr::str_detect(tidy_docs$word, "^[0-9]+$"))
-# turn a tidy tbl into a sparse dgCMatrix
-# note tidylda has support for several document term matrix formats
-d <- tidy_docs %>%
-  cast_sparse(APPLICATION_ID, word, n)
-# let's split the documents into two groups to demonstrate predictions and updates
-d1 <- d[1:50, ]
-d2 <- d[51:nrow(d), ]
-# make sure we have different vocabulary for each data set to simulate the "real world"
-# where you get new tokens coming in over time
-d1 <- d1[, colSums(d1) > 0]
-d2 <- d2[, colSums(d2) > 0]
-set.seed(123)
-lda <- tidylda(
-  dtm = d1,
-  k = 10,
-  iterations = 200,
-  burnin = 175,
-  alpha = 1, # also accepts vector inputs
-  beta = 0.05, # also accepts vector or matrix inputs
-  optimize_alpha = FALSE, # experimental
-  calc_likelihood = TRUE,
-  calc_r2 = TRUE, # see https://arxiv.org/abs/1911.11061
-  return_data = FALSE
-)
 
+set.seed(90210)
 
-#### Begin getting counts for new transfer learning nonsense ----
+# the below calls some internal tidylda functions
+# on style, I'm going to use c++ style variables and be explicit about allocating
+# the variables in a way you don't have to do in R.
+# For example, if I say alpha = 0.1, my R functions know to make alpha = seq(0.1, k)
 
-# begin with dot product to get topic distributions for each document
-theta_hat <- predict(lda, d1, method = "dot", no_common_tokens = "uniform")
+### Initial set up of variables ----
+dtm <- textmineR::nih_sample_dtm
 
-alph <- tidylda:::format_alpha(lda$alpha, nrow(lda$phi))
+Nk <- 10 # number of topics
 
-alph <- matrix(1, ncol = nrow(theta_hat), nrow = ncol(theta_hat)) + alph$alpha
+Nd <- nrow(dtm)
 
-alph <- t(alph)
+Nv <- ncol(dtm)
 
-# get Cd
-Cd <- tidylda:::recover_counts_from_probs(
-  prob_matrix = theta_hat,
-  prior_matrix = alph,
-  total_vector = Matrix::rowSums(d1)
+alpha <- seq(0.1, Nk)
+
+# a matrix b/c of the transfer learning stuff I'm preparing to do
+# FWIW textmineR's sampler takes beta as a matrix too
+beta <- matrix(0.05, ncol = ncol(dtm), nrow = Nk) 
+
+iterations <- 200
+
+# not going to optimize alpha, calculate the likelihood, or do burnin iterations
+# this will keep things simpler
+
+### Allocate some counts to format data as it goes into the C++ gibbs sampler ----
+
+counts <- 
+  tidylda:::initialize_topic_counts(
+    dtm = dtm, 
+    k = Nk,
+    alpha = alpha, 
+    beta = beta,
+    threads = 1
   )
 
-# Cd is done now get Ck 
-Ck <- colSums(Cd)
+# pull out the individual objects as in the C++ gibbs sampler
 
-# use Ck to get counts for Cv
-# in future, you'll have to do this *after* you reconcile vocab and add uniform
-# counts over new words
-bet <- tidylda:::format_beta(beta = lda$beta, k = ncol(Cd), Nv = ncol(d1))
+# docs is a list of integer vectors
+# length(docs[[d]]) == sum(dtm[d, ])
+# the integers correspond to the column indices of dtm, Cv, and beta
+docs <- counts$docs
 
-Cv <- tidylda:::recover_counts_from_probs(
-  prob_matrix = lda$phi,
-  prior_matrix = bet$beta,
-  total_vector = Ck
-)
+docs <- lapply(docs, function(x) x + 1) # r is 1 indexed
+
+# Zd is very similar to docs, except that its indices correspond to topics
+# its entries index the rows of Cv, beta, and Cd (after Cd transpose, below)
+# length(Zd[[d]]) == sum(dtm[d, ])
+Zd <- counts$Zd
+
+Zd <- lapply(Zd, function(x) x + 1) # r is 1 indexed
+
+# Ck is a count of the number of times each topic has been sampled for each
+# instance of a token in the whole corpus
+Ck <- as.numeric(counts$Ck) # as.numeric b/c Ck comes out as a single column matrix
+
+# Cd is the count of topics sampled for each document
+# columns are documents, rows are topics
+Cd <- counts$Cd
+
+# > dim(Cd)
+# [1] 100  10
+
+# Cv is the count of topics sampled for each token
+Cv <- counts$Cv
+
+# > dim(Cv)
+# [1]   10 5210
+
+### Start sampling! ----
+
+# initialize variables used only in the sampler
+
+sum_beta <- sum(beta[1,]) # strict LDA has beta as a vector not a matrix, so sum only a row
+
+sum_alpha <- sum(alpha)
+
+for (t in 1:iterations) { # for each iteration
+  
+  for (d in 1:Nd) { # for each document
+    
+    for (n in 1:length(docs[[d]])) { # for each token in that document
+      
+      # decrement counts from previous iteration where we hit this token
+      # purpose to correctly calculate the probabilities
+      Cd[Zd[[d]][n], d] <- Cd[Zd[[d]][n], d] - 1
+      
+      Cv[Zd[[d]][n], docs[[d]][n]] <- Cv[Zd[[d]][n], docs[[d]][n]] - 1
+      
+      Ck[Zd[[d]][n]] <- Ck[Zd[[d]][n]] - 1
+      
+      # calculate the probability of sampling a topic at this location
+      qz <- numeric(Nk) # initialize topic probability vector
+      
+      for (k in 1:Nk) {
+        qz[k] <- 
+          (Cv[k, docs[[d]][n]] + beta[k, docs[[d]][n]]) / (Ck[k] + sum_beta) *
+          (Cd[k, d] + alpha[k]) / (length(docs[[d]]) + sum_alpha - 1)
+      }
+      
+      # sample a topic at random
+      Zd[[d]][n] <- sample(1:Nk, 1, prob = qz)
+      
+      # increase the counts of Ck, Cd, Cv where we just sampled
+      Cd[Zd[[d]][n], d] <- Cd[Zd[[d]][n], d] + 1
+      
+      Cv[Zd[[d]][n], docs[[d]][n]] <- Cv[Zd[[d]][n], docs[[d]][n]] + 1
+      
+      Ck[Zd[[d]][n]] <- Ck[Zd[[d]][n]] + 1
+      
+    } # end loop over token indices
+    
+  } # end loop over docs
+  
+} # end iterations
 
 
+# At this point, I more or less expel everything from C++ and do residual
+# calculations to properly construct the posterior parameter estimates
